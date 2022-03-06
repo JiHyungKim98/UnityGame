fe35d9569d3601a8c9ba910697f67b5fd9bbba9f
JHKimm <JHKimm@github.com>
2022-03-06 15:39:13 +0900

    JoyStick.cs => 중력 적용, 회전 수정

--- Assets/02. Scripts/JoyStick.cs
+++ Assets/02. Scripts/JoyStick.cs
@@ -9,12 +9,12 @@ public class JoyStick : MonoBehaviour
     //public Transform player; // player pos
     public GameObject player;
     public Animator playerAnimator;
-    public float currentSpeed=0f;
-    public float playerSpeed=0f;
+    public float currentSpeed = 0f;
+    public float playerSpeed = 0f;
     public Transform Stick; // JoyStick
 
     private Vector3 StickFirstPos;
-    private Vector3 JoyVec; 
+    private Vector3 JoyVec;
     private float Radius; // JoyStick Background radius
     private bool playerMoveFlag; // Player move switch
 
@@ -22,7 +22,7 @@ public class JoyStick : MonoBehaviour
     {
         Radius = GetComponent<RectTransform>().sizeDelta.y * 0.5f; // RectTransform y size.
         StickFirstPos = Stick.transform.position; // first stick pos
-        
+
         // fit radius to canvas size
         float Can = transform.parent.GetComponent<RectTransform>().localScale.x;
         Radius *= Can;
@@ -33,6 +33,13 @@ public class JoyStick : MonoBehaviour
     {
         if (playerMoveFlag)
         {
+            Vector3 dir = player.transform.forward * 0.05f;
+            dir.y -= 9.8f;
+            player.GetComponent<CharacterController>().Move(dir);
+            player.transform.eulerAngles = new Vector3(0, Mathf.Atan2(JoyVec.x, JoyVec.y) * Mathf.Rad2Deg, 0);
+            //Vector3 direction = new Vector3(0, Mathf.Atan2(JoyVec.x, JoyVec.y) * Mathf.Rad2Deg, 0);
+            //player.transform.rotation = Quaternion.Slerp(player.transform.rotation,
+            //Quaternion.LookRotation(direction), 0.1f);
             //player.transform.Translate(Vector3.forward * Time.deltaTime * 100f);
             playerAnimator.SetBool("Static_b", false);
             playerAnimator.SetFloat("Speed_f", currentSpeed);
@@ -51,12 +58,12 @@ public class JoyStick : MonoBehaviour
         PointerEventData Data = _Data as PointerEventData;
         Vector3 Pos = Data.position;
 
-        JoyVec = (StickFirstPos-Pos).normalized;
+        JoyVec = (StickFirstPos - Pos).normalized;
 
         // My Current Pos - first JoyStick Pos
         float Distance = Vector3.Distance(Pos, StickFirstPos);
-        
-        
+
+
         if (Distance < Radius)
         {
             Stick.position = StickFirstPos + (-JoyVec) * Distance;
@@ -68,10 +75,11 @@ public class JoyStick : MonoBehaviour
             currentSpeed = 0.2f;
         }
 
-        player.transform.eulerAngles = new Vector3(0, Mathf.Atan2(JoyVec.x, JoyVec.y) * Mathf.Rad2Deg, 0);
+        //player.transform.eulerAngles = new Vector3(0, Mathf.Atan2(JoyVec.x, JoyVec.y) * Mathf.Rad2Deg, 0);
 
 
     }
+
 
     public void DragEnd()
     {
