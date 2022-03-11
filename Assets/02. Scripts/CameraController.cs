ad38bb733ffb38a6400ddf5628da2483f7545502
JHKimm <JHKimm@github.com>
2022-03-11 18:15:09 +0900

    JoyStick.cs => 이전 움직임으로 변경 CameraController.cs => RaycastHit 넣어서 플레이어 가리는 물체 alpha값 조정

--- Assets/02. Scripts/CameraController.cs
+++ Assets/02. Scripts/CameraController.cs
@@ -7,7 +7,8 @@ public class CameraController : MonoBehaviour
     [SerializeField] Camera m_camera = null;
     [SerializeField] private Transform m_targetTrans = null;
     private Transform m_cachedTransform;
-
+    Renderer ObstacleRenderer;
+    Material Mat;
     [SerializeField]
     private Vector3 m_offset = new Vector3(0, 20, -20);
     private float m_smoothTime = 0.3f;
@@ -19,11 +20,56 @@ public class CameraController : MonoBehaviour
         if (m_camera == null) m_camera = GetComponent<Camera>();
         m_cachedTransform = transform;
     }
+    private void Update()
+    {
+        float Distance = Vector3.Distance(transform.position, m_targetTrans.position);
+        Vector3 Direction = (m_targetTrans.transform.position - transform.position).normalized;
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position,Direction,out hit, Distance))
+        {
+            ObstacleRenderer = hit.transform.gameObject.GetComponentInChildren<Renderer>();
+            Mat = ObstacleRenderer.material;
+            if (ObstacleRenderer != null)
+            {
+                Mat.SetFloat("_Mode", 3);
+                Mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                Mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                Mat.SetInt("_ZWrite", 0);
+                Mat.DisableKeyword("_ALPHATEST_ON");
+                Mat.DisableKeyword("_ALPHABLEND_ON");
+                Mat.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+                Mat.renderQueue = 3000;
+                Color matColor = Mat.color;
+                matColor.a = 0.5f;
+                Mat.color = matColor;
 
+            }
+            
+        }
+        else
+        {
+            if (ObstacleRenderer != null)
+            {
+                Mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                Mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                Mat.SetInt("_ZWrite", 1);
+                Mat.DisableKeyword("_ALPHATEST_ON");
+                Mat.DisableKeyword("_ALPHABLEND_ON");
+                Mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                Mat.renderQueue = -1;
+            }
+            
+        }
+    }
     private void LateUpdate()
     {
         if (m_targetTrans != null) SmoothFollow();
     }
+
+
+
+
+
 
     private void SmoothFollow()
     {
